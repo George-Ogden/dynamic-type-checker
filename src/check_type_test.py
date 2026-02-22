3b34b118d6c96b02de131e5b96b115e89bf498e3
Claire Cooper <clairecooper112@gmail.com>
2026-02-22 15:45:07 +0000

    Qualify Any type as typing.Any in check_type_test

--- src/check_type_test.py
+++ src/check_type_test.py
@@ -1,5 +1,6 @@
 import sys
 import types
+import typing
 from typing import Any
 
 from debug import pprint
@@ -94,10 +95,10 @@ class CustomMetaclassSubclassIsInstance(CustomMetaclassIsInstance): ...
         (types.NoneType, None, True),
         (types.NoneType, 3.1, False),
         # Any type hint
-        (Any, 1, True),
-        (Any, None, True),
-        (Any, OneFieldClass(1), True),
-        (Any, OneFieldClass, True),
+        (typing.Any, 1, True),
+        (typing.Any, None, True),
+        (typing.Any, OneFieldClass(1), True),
+        (typing.Any, OneFieldClass, True),
     ],
 )
 def test_check_type(typ: Any, obj: object, succeeds: bool) -> None:
