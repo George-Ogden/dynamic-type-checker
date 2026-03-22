82d825977ac7c4e85ce2dede35da34f2a95b23ea
Claire Cooper <clairecooper112@gmail.com>
2026-03-22 17:54:49 +0000

    Extract makemsg method in MalformedTypeError

--- src/errors.py
+++ src/errors.py
@@ -3,13 +3,16 @@ from .utils import TypeAnnotation
 
 class MalformedTypeError(TypeError):
     def __init__(self, typ: TypeAnnotation, *, extra_msg: str = "") -> None:
-        msg = f"{typ} is not a valid type."
         self.typ = typ
+        super().__init__(self.make_msg(extra_msg))
+
+    def make_msg(self, extra_msg: str) -> str:
+        msg = f"{self.typ} is not a valid type."
         if extra_msg:
             assert extra_msg == extra_msg.strip()
             assert extra_msg[-1] in ".!?"
             msg += f" {extra_msg}"
-        super().__init__(msg)
+        return msg
 
     @classmethod
     def requires_arguments_message(cls, typ: TypeAnnotation) -> str:
