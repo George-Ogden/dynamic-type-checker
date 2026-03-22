77b3a965204b41c1368b3e1b22062afb15350283
Claire Cooper <clairecooper112@gmail.com>
2026-03-22 17:49:51 +0000

    Add typ attribute to MalformedTypeError

--- src/errors.py
+++ src/errors.py
@@ -4,6 +4,7 @@ from .utils import TypeAnnotation
 class MalformedTypeError(TypeError):
     def __init__(self, typ: TypeAnnotation, *, extra_msg: str = "") -> None:
         msg = f"{typ} is not a valid type."
+        self.typ = typ
         if extra_msg:
             assert extra_msg == extra_msg.strip()
             assert extra_msg[-1] in ".!?"
